42d84b2e9e3ac0dfc9a3147f4dcf6dac5c3dfba0
hedy-6 <hedy_6@163.com>
2020-12-02 13:40:04 +0800

    fix: 防抖节流

--- docs/js/debounce.js
+++ docs/js/debounce.js
@@ -1,9 +1,15 @@
-function debounce(fn) {
+// 防抖
+function debounce(fn, delay) {
   let timeout = null;
-  return function(params) {
-    clearTimeout(timeout);
+  return function() {
+    if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => {
       fn.apply(this, arguments);
-    }, 500);
+    }, delay);
   };
 }
+/* 防抖应用场景：
+1. 登陆、发短信等按钮；
+2. 浏览器窗口大小；
+3. 文本编辑器实时保存；
+*/
--- docs/js/throttle.js
+++ docs/js/throttle.js
@@ -1,11 +1,17 @@
-function throttle(fn) {
-  let canrun = true;
-  return function {
-    if (!canrun) return
-    canrun = false
+// 节流
+function throttle(fn, delay) {
+  let isCan = true;
+  return function() {
+    if (!isCan) return;
+    isCan = false;
     setTimeout(() => {
-      fn.apply(this, arguments)
-      canrun= true
-    }, 500);
-  }
+      fn.apply(this, arguments);
+      isCan = true;
+    }, delay);
+  };
 }
+/* 节流应用场景：
+1. scroll 事件；
+2. 浏览器播放事件；
+3. input 实时搜索；
+*/
